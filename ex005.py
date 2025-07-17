e7cd6c0c499c9708b7b3434c26535eda2dd44cd9
LQueirozz <luis9queiroz@gmail.com>
2025-07-17 16:14:53 -0300

    ex 005

---
+++ ex005.py
@@ -0,0 +1,5 @@
+#Exercício 5: Antecessor e Sucessor
+#Aprendendo sobre operadores pt1
+
+n= int(input('Digite um número inteiro: '))
+print('Analisando o número {}, seu antecessor é {} e seu sucessor é {}' .format(n, n-1, n+1))
