aef5eb356570b17a003bb4a9a9a5525b5fb60222
LQueirozz <luis9queiroz@gmail.com>
2025-07-17 15:37:43 -0300

    ex 003

---
+++ ex003.py
@@ -0,0 +1,7 @@
+#Exercício 3: Soma de dois valores inteiros
+#Aprendendo a usar diferentes tipos de dados básicos
+
+n1 = int( input('Digite um número inteiro: '))
+n2 = int( input('Digite mais um número inteiro: '))
+s1= n1+n2
+print('a soma entre {} e {} dá o valor {}' .format(n1,n2,s1) )
