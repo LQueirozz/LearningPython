e4427312d76a6840c4ca1b8cb6a87a872fb8c020
LQueirozz <luis9queiroz@gmail.com>
2025-07-17 17:50:24 -0300

    ex 014

---
+++ ex014.py
@@ -0,0 +1,7 @@
+#Exercício 14: Conversão de temperatura
+#Aprendendo sobre operadores pt10
+
+celsius = float( input('Digite a temperatura em ºC:'))
+fahrenheit = (1.8*celsius) +32
+
+print('A temperatura {:.2f}ºC corresponde à {:.2f}ºF!' .format(celsius, fahrenheit))
