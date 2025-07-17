a06d7906e72793b1a1ded3653761f4b10398f0ef
LQueirozz <luis9queiroz@gmail.com>
2025-07-17 16:33:59 -0300

    ex 007

---
+++ ex007.py
@@ -0,0 +1,9 @@
+#Exercício 7: Média Aritmética
+#Aprendendo sobre operadores pt3
+
+n1 = float( input('A primeira nota do aluno foi: ') )
+n2 = float( input('A segunda nota do aluno foi: ') )
+
+med= (n1+n2)/2
+
+print('A média aritmética do aluno foi: {}' .format(med))
