523369891fd7fb5fb5d37c95a11cebe18ce94315
LQueirozz <luis9queiroz@gmail.com>
2025-07-17 17:42:52 -0300

    ex 012

---
+++ ex012.py
@@ -0,0 +1,6 @@
+#Exercício 12: Descontos
+#Aprendendo sobre operadores pt8
+
+n= float( input('Digite o preço de um produto: R$'))
+
+print(f"O produto que custava {n:.2f}, na promoção com 5% de desconto fica {(n*0.95):.2f}" )
