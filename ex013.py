6cc688c1e7544d07994727acd995db190a6e63c8
LQueirozz <luis9queiroz@gmail.com>
2025-07-17 17:44:50 -0300

    ex 013

---
+++ ex013.py
@@ -0,0 +1,6 @@
+#Exercício 13: Reajuste salarial
+#Aprendendo sobre operadores pt9
+
+n= float( input('Digite o salário do trabalhador: R$'))
+
+print(f"O trabalhador que recebia {n:.2f}, com um aumento de 15% vai começar a receber {(n*1.15):.2f}" )
