5ba1517c38352ab65a5cd74af7b4526fd7332c1c
LQueirozz <luis9queiroz@gmail.com>
2025-07-17 21:50:29 -0300

    ex 016

---
+++ ex016.py
@@ -0,0 +1,8 @@
+#Exercício 16: Quebrando um número
+#Aprendendo sobre importações pt1
+
+from math import floor
+
+n= float( input('Digite um número real qualquer: ') )
+
+print('A parte inteira do número {} é {}' .format(n, floor(n)))
