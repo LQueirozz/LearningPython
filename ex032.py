419cea26d00b3f3cf038aeaeb07193231e97ee1f
LQueirozz <luis9queiroz@gmail.com>
2025-07-19 22:56:52 -0300

    ex 032

---
+++ ex032.py
@@ -0,0 +1,10 @@
+#Exercício 32: Ano bissexto
+#Aprendendo sobre condicionais pt5
+
+ano=int(input('Digite um ano qualquer: '))
+
+if(ano%4==0 and ano%100!=0):
+    print('O ano {} é bissexto!' .format(ano))
+
+else:
+    print('O ano {} não é bissexto!' .format(ano))
