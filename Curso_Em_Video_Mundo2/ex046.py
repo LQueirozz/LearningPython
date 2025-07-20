3c6bba51ae3ad9f48b7a4b46f61efb5998655ee2
LQueirozz <luis9queiroz@gmail.com>
2025-07-20 01:23:22 -0300

    ex 046

---
+++ Curso_Em_Video_Mundo2/ex046.py
@@ -0,0 +1,10 @@
+#Exercício 46: Contagem regressiva
+#Aprendendo sobre for loops pt1
+
+from time import sleep
+
+for i in range(10, 0, -1):
+    print(i)
+    sleep(1)
+
+print("Feliz ano Novo!!!")
