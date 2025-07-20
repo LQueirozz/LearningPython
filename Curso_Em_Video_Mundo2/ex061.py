2b1d99523885b4c5b88fd376f42a179626641783
LQueirozz <luis9queiroz@gmail.com>
2025-07-20 16:48:33 -0300

    ex 061

---
+++ Curso_Em_Video_Mundo2/ex061.py
@@ -0,0 +1,13 @@
+#Exercício 51: Progressão aritmética com while
+#Aprendendo sobre while loops pt5
+
+print('Informe a razão e o primeiro termo de uma progressão aritmética')
+r=int(input('Razão: '))
+a0= float(input('Primeiro termo:'))
+a=a0
+i=0
+
+while(a!= (a0 + 9*r)):
+    a= a0 + i*r
+    print('Termo a{}: {:.2f}' .format(i,a))
+    i+=1
