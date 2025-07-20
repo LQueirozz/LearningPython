f00edff6c05613c3bbf39b12cee62c280f01cf85
LQueirozz <luis9queiroz@gmail.com>
2025-07-20 15:10:37 -0300

    ex057.py

--- Curso_Em_Video_Mundo2/ex057.py
+++ Curso_Em_Video_Mundo2/ex057.py
@@ -2,9 +2,9 @@
 #Aprendendo sobre while loops pt1
 
 
-a=((input('Digite qual o seu sexo (M, F ou I): ')).strip()).upper()
+a = ((input('Digite qual o seu sexo (M, F ou I): ')).strip()).upper()
 
 while( (a!='F') and (a!='M') and (a!='I') ):
-    a=((input('Opção inválida! Favor escolher uma das três opções fornecidas (M, F ou I): ')).strip()).upper()
+    a = ((input('Opção inválida! Favor escolher uma das três opções fornecidas (M, F ou I): ')).strip()).upper()
 
 print('Opção escolhida: {}' .format(a))
