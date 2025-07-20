bf73e73f4eb3a032670f36fab72b20bd8d6d2967
LQueirozz <luis9queiroz@gmail.com>
2025-07-19 23:10:57 -0300

    Hotfix

--- Curso_Em_Video_Mundo1/ex002.py
+++ Curso_Em_Video_Mundo1/ex002.py
@@ -1,5 +1,5 @@
 #Exercício 2: Lidando com input de usuário
 #Aprender como usar input
 
-nome = input("digite seu nome, por favor ")
+nome = input("digite seu nome, por favor ")                 
 print("É um prazer te conhecer, {}" .format(nome) )
