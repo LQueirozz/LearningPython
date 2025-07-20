a3c2e08e34661d8b336d7e96bc3b4e9c5044d839
LQueirozz <luis9queiroz@gmail.com>
2025-07-19 21:43:09 -0300

    ex 027

---
+++ ex027.py
@@ -0,0 +1,8 @@
+#Exercício 26: Primeiro e último nome
+#Aprendendo sobre manipulação de strings pt6
+
+nome = ( ( ( input("Qual o seu nome?: ") ).strip() ).lower() ).split()
+
+print('Przer em te conhecer!')
+print('O seu primeiro nome é {}' .format(nome[0]))
+print('O seu último nome é {}' .format(nome[len(nome) -1]))
