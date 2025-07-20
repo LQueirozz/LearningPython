84ff7c9ec6055e38ff30fb528f1f1065ead2565e
LQueirozz <luis9queiroz@gmail.com>
2025-07-19 21:43:37 -0300

    ex 027

--- ex027.py
+++ ex027.py
@@ -3,6 +3,6 @@
 
 nome = ( ( ( input("Qual o seu nome?: ") ).strip() ).lower() ).split()
 
-print('Przer em te conhecer!')
+print('Prazer em te conhecer!')
 print('O seu primeiro nome é {}' .format(nome[0]))
 print('O seu último nome é {}' .format(nome[len(nome) -1]))
